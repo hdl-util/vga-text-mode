f31cd3aa8915cc6da4102b854248ce46b6756b74
Sameer Puri <11097096+sameer@users.noreply.github.com>
2020-03-29 13:41:14 -0400

    Fix Verilog assignment truncation warnings

--- src/console.v
+++ src/console.v
@@ -19,7 +19,7 @@ wire [23:0] fgrgb, bgrgb;
 wire blink;
 attributemap attributemap(.attribute(attribute), .fgrgb(fgrgb), .bgrgb(bgrgb), .blink(blink));
 
-reg [9:0] prevcy = 0;
+    reg [BIT_HEIGHT-1:0] prevcy = 0;
 reg [$clog2(FONT_HEIGHT)-1:0] vindex = 0;
 reg [$clog2(FONT_WIDTH)-1:0] hindex = 0;
 reg [5:0] blink_timer = 0;
@@ -36,12 +36,12 @@ begin
     else if (prevcy != cy)
     begin
         prevcy <= cy;
-        vindex <= vindex == FONT_HEIGHT - 1 ? 0 : vindex + 1'b1;
+        vindex <= vindex == FONT_HEIGHT - 1 ? 1'b0 : vindex + 1'b1;
         hindex <= 0;
     end
     else
     begin
-        hindex <= hindex == FONT_WIDTH - 1 ? 0 : hindex + 1'b1;
+        hindex <= hindex == FONT_WIDTH - 1 ? 1'b0 : hindex + 1'b1;
     end
 
     if (blink && blink_timer[5])
